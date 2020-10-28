c4cf5e50bdce1a9f0c916db23b0df9060d83500b
Mark Leung <mark.leung89@gmail.com>
2020-10-28 13:25:45 +1100

    Read user input functionality

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 console.log("TOY ROBOT CHALLENGE");
+
+// **********************************
+//   ReadLine Input - User Interface
+// **********************************
+
+const readline = require('readline')
+
+// Initialize input receiver
+const readLine = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout
+});
+
+// Retrieve User Input
+readLine.on("line", (input) => { 
+    readLine.prompt(); 
+});
+
+// Set Ending Prompt
+readLine.on("close", () => {
+    console.log("\nGOODBYE !!!");
+    process.exit(0);
+});
+
+// Trigger User Prompt
+readLine.setPrompt('ToyRobo> ');
+readLine.prompt();
