c15bd0696dd24bb96325779fa629ec62caa959d6
Mark Leung <mark.leung89@gmail.com>
2020-10-28 18:14:25 +1100

    Add validation & error messages

---
+++ src/validations/errorMessages.js
@@ -0,0 +1,2 @@
+export const invalidDirection = "That is an invalid direction";
+export const invalidPlacement = "Are you trying to kill me?";
---
+++ src/validations/validations.js
@@ -0,0 +1,18 @@
+import { invalidDirection, invalidPlacement } from "./errorMessages";
+import directions from "../constants/directions";
+
+export const isValidMove = (x, xMaxLength, y, yMaxLength) => {
+    if ( (x >= xMaxLength ) || (x < 0) || (y < 0) || (y >= yMaxLength )) {
+        console.log(invalidPlacement);
+        return false;
+    }
+    return true;
+}
+
+export const isValidDirection = (direction) => {
+    if ( !directions[direction]) {
+        console.log(invalidDirection);
+        return false;
+    }
+    return true;
+}
