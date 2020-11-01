d4991558e49971156d0d634a770c2aad9256c4b0
Mark Leung <mark.leung89@gmail.com>
2020-11-01 22:16:56 +1100

    Add test cases for validations

--- test/validations/validations.test.js
+++ test/validations/validations.test.js
@@ -3,8 +3,10 @@
 *****************************************************/
 
 import { expect } from 'chai';
-import { isValidMove, isValidDirection } from "../../src/validations/validations";
+import { isValidMove, isValidDirection, isValidPlaceArgs, isRobotOnTable } from "../../src/validations/validations";
 import directions from "../../src/constants/directions";
+import Robot from "../../src/components/robot";
+import Table from "../../src/components/table";
 
 describe("validations - Valid Moves", function() {
       // [x,y, xMaxLength, yMaxLength]
@@ -49,5 +51,55 @@ describe("validations - Invalid Directions", function() {
         let isFalse = isValidDirection(direction);
         expect(isFalse).to.be.equal(false);
       });
+    });
+});
+
+describe("validations - Place Args", function() {
+    
+    it(`Place Args True`, function() {
+      let isTrue = isValidPlaceArgs("1,2,NORTH");
+      expect(isTrue).to.be.equal(true);
+    });
+
+    it(`Place Args False`, function() {
+      let isFalse = isValidPlaceArgs("1");
+      expect(isFalse).to.be.equal(false);
+    });
+
+    it(`Place Args False 2`, function() {
+      let isFalse = isValidPlaceArgs("1,2");
+      expect(isFalse).to.be.equal(false);
+    });
+
+    it(`Place Args - X not Int`, function() {
+      let isFalse = isValidPlaceArgs("TEST,2");
+      expect(isFalse).to.be.equal(false);
+    });
+
+    it(`Place Args - Y not Int`, function() {
+      let isFalse = isValidPlaceArgs("1,TEST");
+      expect(isFalse).to.be.equal(false);
+    });
+});
+
+describe("validations - robot on table", function() {
+
+    // Initialize table
+    let length = 5;
+    let width = 5;
+    let board = new Table(length, width);
+
+    // Initialize robot
+    let robot = new Robot();
+
+    it(`Robot not on table`, function() {
+      let isFalse = isRobotOnTable(robot);
+      expect(isFalse).to.be.equal(false);
+    });
+
+    it(`Robot on table`, function() {
+      robot.setTable(board);
+      let isTrue = isRobotOnTable(robot);
+      expect(isTrue).to.be.equal(true);
     });
 });
